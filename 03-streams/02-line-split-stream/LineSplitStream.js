2d2ad4cbedcf7aba53fa77e574e2b5e10a2adf2e
Sergii Pukhalskyi <sergiip@localize.city>
2021-12-08 00:42:26 +0200

    Split by line module-3 task-2

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -3,13 +3,42 @@ const os = require('os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
+    options = Object.assign({}, options, {
+      decodeStrings: false,
+    });
     super(options);
+
+    this.strInMem = '';
   }
 
   _transform(chunk, encoding, callback) {
+    let line = '';
+
+    const eolParser = (str) => {
+      const idx = str.indexOf(os.EOL);
+
+      if (idx >= 0) {
+        const beforeEOL = str.slice(0, idx);
+        const afterEOL = str.slice(idx + 1);
+
+        this.push(beforeEOL);
+
+        eolParser(afterEOL);
+      } else {
+        line += str;
+      }
+    };
+
+    eolParser(this.strInMem + chunk);
+    this.strInMem = line;
+    callback();
   }
 
   _flush(callback) {
+    if (this.strInMem) {
+      this.push(this.strInMem);
+    }
+    callback();
   }
 }
 
