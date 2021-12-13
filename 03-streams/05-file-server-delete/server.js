2eac735015e42c446ca01cb834c56c5da65af1e8
Sergii Pukhalskyi <sergiip@localize.city>
2021-12-13 22:27:42 +0200

    Delete file module-3 task-5

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,4 +1,4 @@
-const url = require('url');
+const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
@@ -10,9 +10,36 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const responseHandler = (errCode) => {
+    res.statusCode = errCode;
+    res.end();
+  };
+
+  const removeFile = () => {
+    fs.unlink(filepath, (err) => {
+      if (err) console.log('No file to remove');
+      console.log(`${filepath} was deleted`);
+    });
+  };
+
+  req.on('error', () => {
+    responseHandler(500);
+  });
+
   switch (req.method) {
     case 'DELETE':
-
+      if (pathname.indexOf('/') >= 0) {
+        responseHandler(400);
+        break;
+      }
+      fs.access(filepath, fs.F_OK, (err) => {
+        if (err) {
+          responseHandler(404);
+        } else {
+          removeFile();
+          responseHandler(200);
+        }
+      });
       break;
 
     default:
