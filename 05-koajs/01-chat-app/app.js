4773f1f80d12bb9c28f08a9c07ab397d34c4c71b
Sergii Pukhalskyi <sergiip@localize.city>
2021-12-18 23:22:14 +0200

    Koa chat. Module-5 task-1

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -9,9 +9,28 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+  await new Promise((resolve) => {
+    const messageResolver = (message) => {
+      ctx.body = message;
+      resolve(messageResolver);
+    };
+
+    app.on('message', messageResolver);
+  }).then((mResolver) => {
+    app.off('message', mResolver);
+  });
+  next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (!ctx.request.body.message) {
+    ctx.body = null;
+    next();
+  } else {
+    next();
+    ctx.app.emit('message', ctx.request.body.message);
+    ctx.body = 'sent';
+  }
 });
 
 app.use(router.routes());
